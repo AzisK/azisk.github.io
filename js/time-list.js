f68909e44900ff51f2c4494986d06ae3f9842b15
AzisK <azuolas.krusna@yahoo.com>
2017-10-09 11:09:35 +0300

    Add resume styling and rename some classes

--- js/time-list.js
+++ js/time-list.js
@@ -40,12 +40,12 @@ addToList(educationList, 'Erasmus studies', 'Bilkent', 'http://w3.bilkent.edu.tr
 var education = document.querySelector('.education-list');
 education.innerHTML = list(educationList);
 
-var workList = [];
-
-addToList(workList, 'Software Engineer', 'Icybit', '', '2017 April - September');
-addToList(workList, 'Software Engineer Intern', 'Valuetech', '', '2017 January - April');
-addToList(workList, 'Junior Laboratory Coordinator', 'KTU', '', '2016 Autumn');
-addToList(workList, 'Server', 'P F Changs Hawaii', '', '2016 Summer');
-
-var work = document.querySelector('.work-list');
-work.innerHTML = list(workList);
+// var workList = [];
+//
+// addToList(workList, 'Software Engineer', 'Icybit', '', '2017 April - September');
+// addToList(workList, 'Software Engineer Intern', 'Valuetech', '', '2017 January - April');
+// addToList(workList, 'Junior Laboratory Coordinator', 'KTU', '', '2016 Autumn');
+// addToList(workList, 'Server', 'P F Changs Hawaii', '', '2016 Summer');
+//
+// var work = document.querySelector('.work-list');
+// work.innerHTML = list(workList);
